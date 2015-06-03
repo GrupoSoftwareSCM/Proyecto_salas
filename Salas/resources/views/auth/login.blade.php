a37056033a9dbd04aa4f91061fc1b6d25e681cd1
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2015-06-03 14:18:51 -0300

    responsive

--- Salas/resources/views/auth/login.blade.php
+++ Salas/resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
+		<div class="col-md-5 col-md-offset-3 col-sm-7">
 			<div class="panel panel-default">
 				<div class="panel-heading">						
 						<center><img src="/img/Logo_UTEM.png" style="width:140px;height:150px;"></center> <!-- height = alto
@@ -27,20 +27,14 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">
-								<b>R.U.T. (Dirdoc)</b>
-							</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+							<div class="col-md-6 col-md-offset-3">
+								<input type="email" class="form-control" name="email" placeholder="RUT">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">
-								<b>Contraseña (Dirdoc)</b>
-							</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+							<div class="col-md-6 col-md-offset-3">
+								<input type="password" class="form-control" name="password" placeholder="Contraseña Dirdoc">
 							</div>
 						</div>
 
