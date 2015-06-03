976a9434d00992322855efab939b4bf8c78cea21
jean pierre cid <jeanpierre.cid@gmail.com>
2015-06-03 19:12:44 -0300

    Subiendo rutas

--- Salas/app/Http/routes.php
+++ Salas/app/Http/routes.php
@@ -16,15 +16,15 @@
 Route::get('/', 'HomeController@index');
 
 Route::get('adm','Administrador\AdmUserController@index');
-<<<<<<< HEAD
+
 Route::get('alum','Alumnos\AlumUserController@index');
 Route::get('alum/horario','Alumnos\AlumUserController@horario');
 Route::get('alum/salas','Alumnos\AlumUserController@salas');
-=======
+
 Route::get('adm/modif','Administrador\AdmUserController@index');
 Route::get('adm/archivar','Administrador\AdmUserController@index');
 Route::get('adm/crear','Administrador\AdmCrearController@index');
->>>>>>> 26a8804de5c477dc42cc23baaa2ab33aacb4e2c9
+
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
