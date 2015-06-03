26a8804de5c477dc42cc23baaa2ab33aacb4e2c9
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2015-06-03 19:05:00 -0300

    agrueg vistas para el admin

--- Salas/resources/views/Administrador/homeAdm.blade.php
+++ Salas/resources/views/Administrador/homeAdm.blade.php
@@ -2,13 +2,32 @@
 
 @section('content')
 <div class="col-md-8 col-md-offset-2"> 
-	@if(true) <!-- ver si el admin esta conectado	-->	
+	<?php if($_SERVER['REQUEST_URI'] == "/adm") {?>
 		<div class="panel panel-default">
 			<div class="panel-body">
 			Bienvenido admnistrador, en esta pagina usted podra ..............................
 			</div>
 		</div>
-@endif
+	<?php } ?>
+	<?php if($_SERVER['REQUEST_URI'] == "/adm/crear") {?>
+		<div class="panel panel-default">
+			<div class="panel-body">
+			estamos en crear 
+			</div>
+		</div>
+	<?php } ?>
+	<?php if($_SERVER['REQUEST_URI'] == "/adm/modif") {?>
+		<div class="panel panel-default">
+			<div class="panel-body"> 
+			</div>
+		</div>
+	<?php } ?>
+	<?php if($_SERVER['REQUEST_URI'] == "/adm/archivar") {?>
+		<div class="panel panel-default">
+			<div class="panel-body">
+			</div>
+		</div>
+	<?php } ?>
 </div>
 
-@endsection
+@stop
