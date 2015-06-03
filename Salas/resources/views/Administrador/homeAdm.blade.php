ee839443cf69111978c6a0037284c95a4ecc8bfe
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2015-06-03 17:27:48 -0300

    quite if

--- Salas/resources/views/Administrador/homeAdm.blade.php
+++ Salas/resources/views/Administrador/homeAdm.blade.php
@@ -2,15 +2,13 @@
 
 @section('content')
 <div class="col-md-8 col-md-offset-2"> 
-	@if(url('/adm')) <!-- ver si el admin esta conectado	-->	
+	@if(true) <!-- ver si el admin esta conectado	-->	
 		<div class="panel panel-default">
 			<div class="panel-body">
 			Bienvenido admnistrador, en esta pagina usted podra ..............................
 			</div>
 		</div>
-	@if(url('/adm/create'))
-		hola
-	@endif
+@endif
 </div>
 
 @endsection
