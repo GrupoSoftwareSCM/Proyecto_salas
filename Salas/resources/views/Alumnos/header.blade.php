705d5d63654d5a2eabf961bce68985786a2f1790
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2015-06-04 02:11:16 -0300

    borre archivos y deje solo el home, error del css

--- Salas/resources/views/Alumnos/header.blade.php
+++ Salas/resources/views/Alumnos/header.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="{{URL::asset('css/bootstrap.min.css')}}" rel="stylesheet" media="screen">
 	<title>Bienvenido</title>
 </head>
 <body>
@@ -18,15 +18,15 @@
 			  	<div class="panel-body">
 			  		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav">
-							<li><a href="{{ url('/alum')}}">Inicio</a></li>
-							<li><a href="{{ url('/alum/horario')}}">Horario</a></li>
-							<li><a href="{{ url('/alum/salas')}}">Salas</a></li>
+							<li><a href="{{ url('alum')}}">Inicio</a></li>
+							<li><a href="{{ url('alum/horario')}}">Horario</a></li>
+							<li><a href="{{ url('alum/salas')}}">Salas</a></li>
 						</ul>
 					</div>
 			  	</div>
 			</div>
 		</div>
-		@yield('content')
 	</div>
+	@yield('content')
 </body>
 </html>
--- Salas/resources/views/Alumnos/homeAlum.blade.php
+++ Salas/resources/views/Alumnos/homeAlum.blade.php
@@ -2,15 +2,26 @@
 
 
 @section('content')
-
-<div class="col-md-8 col-md-offset-2"> 
-	@if(true) <!-- ver si el admin esta conectado	-->	
+<!--{{$_SERVER['REQUEST_URI']}}-->
+<div class="row">
+	<div class="col-md-8 col-md-offset-2"> 
 		<div class="panel panel-default">
 			<div class="panel-body">
-			Bienvenido, en esta pagina usted podra reconocer su horario con sus respectivas salas de clases o conocer las asignaciones de salas de cada ramo por cada dia de la semana.........
+				@if($_SERVER['REQUEST_URI'] == "/alum")
+					Bienvenido, en esta pagina usted podra reconocer su horario con sus respectivas salas de clases o conocer las asignaciones de salas de cada ramo por cada dia de la semana.........
+				@endif
+
+				@if($_SERVER['REQUEST_URI'] == "/alum/horario")
+					Bienvenido, en esta pagina usted podra reconocer su horario 
+				@endif
+
+				@if($_SERVER['REQUEST_URI'] == "/alum/salas")
+					Bienvenido, en esta pagina usted podra reconocer su salas
+				@endif
+				
 			</div>
 		</div>
-@endif
+	</div>
 </div>
 
 @endsection
