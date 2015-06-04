1a57e1dfb3e972db4f49907f2624a42eb1b0cc35
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2015-06-04 12:22:14 -0300

    agregue rutas faltantes a docente

--- Salas/app/Http/routes.php
+++ Salas/app/Http/routes.php
@@ -15,17 +15,23 @@
 
 Route::get('/', 'HomeController@index');
 
+//RUTAS PARA EL DOCENTE
 Route::get('doc','Docente\DocUserController@index');
+Route::get('doc/clases','Docente\DocUserController@index');
+Route::get('doc/salas','Docente\DocUserController@index');
+
+//RUTAS PARA EL ENCARGADO DE CAMPUS
 Route::get('encar','Encargado\EncarUserController@index');
 Route::get('encar/asig','Encargado\EncarUserController@index');
 Route::get('encar/modif','Encargado\EncarUserController@index');
 Route::get('encar/ingre','Encargado\EncarUserController@index');
 
-
+//RUTAS PARA EL ALUMNOS
 Route::get('alum',        'Alumnos\AlumUserController@index');
 Route::get('alum/horario','Alumnos\AlumUserController@index');
 Route::get('alum/salas',  'Alumnos\AlumUserController@index');
 
+//RUTAS PARA EL ADMINISTRADOR
 Route::get('adm',         'Administrador\AdmUserController@index');
 Route::get('adm/modif',   'Administrador\AdmUserController@index');
 Route::get('adm/archivar','Administrador\AdmUserController@index');
