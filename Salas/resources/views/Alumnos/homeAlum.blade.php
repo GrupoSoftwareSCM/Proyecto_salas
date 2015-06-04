b41bd9d7832463da3093912ed3096cfe03f969e8
jean pierre cid <jeanpierre.cid@gmail.com>
2015-06-04 17:51:35 -0300

    Rutas y vistas de encargado de campus

--- Salas/resources/views/Alumnos/homeAlum.blade.php
+++ Salas/resources/views/Alumnos/homeAlum.blade.php
@@ -13,32 +13,150 @@
 
 				@if($_SERVER['REQUEST_URI'] == "/alum/horario")
 
-				  <div class="col-lg-6">
-    <div class="input-group">
-      <input type="text" class="form-control">
- 
-      <div class="input-group-btn">
-        <button type="button" class="btn btn-default dropdown-toggle"
-                data-toggle="dropdown">
-          Acción <span class="caret"></span>
-        </button>
- 
-        <ul class="dropdown-menu pull-right" role="menu">
-          <li><a href="#">Acción 1</a></li>
-          <li><a href="#">Acción #2</a></li>
-          <li><a href="#">Acción #3</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Acción #4</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
+			       
 					Bienvenido, en esta pagina usted podra reconocer su horario 
+					<table id="sample-table-1" class="table table-striped table-bordered table-hover">
+                            <h1>
+
+                            </h1>
+                            <thead>
+                                <tr>
+                                    <th class="center" ></th>
+                                    <th class="center" width="150px">Lunes</th>
+                                    <th class="center" width="150px">Martes</th>
+                                    <th class="center" width="150px">Miércoles</th>
+                                    <th class="center" width="150px">Jueves</th>
+                                    <th class="center" width="150px">Viernes</th>
+                                    <th class="center" width="150px">Sábado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                                                <tr class="programa">
+                                    <td align="center">08:00 - 09:30 - I</td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <tr class="programa">
+                                    <td align="center">09:40 - 11:10 - II</td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>                    
+                                    <tr class="programa">
+                                    <td align="center">11:20 - 12:50 - III</td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+                                    <tr class="programa">
+                                    <td align="center">13:00 - 14:30 - IV</td>
+                                     <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+                                    <tr class="programa">
+                                    <td align="center">14:40 - 16:10 - V</td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+                                    <tr class="programa">
+                                    <td align="center">16:20 - 17:50 - VI</td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>         
+                                    <tr class="programa">
+                                    <td align="center">18:00 - 19:30 - VII</td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>             
+                                    <tr class="programa">
+                                    <td align="center">19:00 - 20:30 - VIII</td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" ></td>
+                                    <td class="td" align="center" >  </td>
+                                    <tr class="programa">                             
+                                    </tr>
+                            </tbody>
+                        </table>
 				@endif
 
 				@if($_SERVER['REQUEST_URI'] == "/alum/salas")
-					Bienvenido, en esta pagina usted podra reconocer su salas
+					<div><h3>ASIGNACIONES DE SALAS</div></h3>				
+		            <div>
+		            <br>	
+		            </div>
+		            <div class="row"> 
+                    <div class="col-md-8">
+                    	
+                        <div class="form-group"> <div class="col-md-8"> <b>CAMPUS</b></div>
+                        <div class="col-md-4"> <datalist id="listas">
+                            <option value="Macul">
+                            <option value="Fae">
+                            <option value="390">
+                            <option value="Casa central">
+                         </datalist>
+                    <input name="Campus" list="listas">
+                    </div>
+                      </div> 
+                          <div class="form-group"><div class="col-md-8"> <b> DIA </b></div>   
+                          <div class="col-md-4"><datalist id="listas2">
+                            <option value="Lunes">
+                            <option value="Martes">
+                            <option value="Miercoles">
+                            <option value="Jueves">
+                            <option value="Viernes">
+                            <option value="Sabado">
+                         </datalist>
+                    <input name="Dia" list="listas2">
+                     </div>
+                       </div>
+                          <div class="form-group"> <div class="col-md-8"> <b>PERIODO</b></div>   
+                         <div class="col-md-4"><datalist id="listas3">
+                            <option value="08:00 - 9:30">
+                            <option value="09:45 - 11:10">
+                            <option value="11:20 - 12:50">
+                            <option value="13:00 - 14:30">
+                            <option value="14:40 - 16:10">
+                            <option value="16:20 - 17:50">
+                            <option value="18:00 - 19:30">
+                            <option value="19:40 - 21:10">
+                         </datalist>
+                    <input name="Periodo" list="listas3">
+                    </div>
+                    </div>
+                    </form>
+
+                    </div>
+		            </div>
+                     <br><br>
+   <div class="btn-group">
+  <div class="col-md-8"><button type="button" class="btn btn-primary">ACEPTAR</button>
+  </div></div>
+                    
+ 
+  
+</div>
 				@endif
 				
 			</div>
--- Salas/resources/views/Encargado/homeEncar.blade.php
+++ Salas/resources/views/Encargado/homeEncar.blade.php
@@ -15,11 +15,47 @@
 				@endif
 
 				@if($_SERVER['REQUEST_URI'] == "/encar/modif")
-					Modificar aspectos como la capacidad y nombre de salas.
+					<h2>Modificar sala</h2>
+					<div class="row">
+						
+					  	<form name="formulario" id="formulario" action="/demos/2013/05-html-form-datalist.php" method="POST">
+                        <div class="form-group">
+                        <div class="col-md-4"><b>SALA</b></div>
+                         <div class="col-md-8"> <datalist id="salas">
+                            <option value="M1....">
+                            <option value="M2....">
+                            <option value="M3....">
+                            <option value="M4....">
+                         </datalist>
+                    <input name="SALA" list="salas">
+                        </div>
+                        <div>
+                        <div class="col-md-4"><label for="date">CAPACIDAD</label></div>
+                        <div class="col-md-8"><input type="number" value="1" min="1" max="50"></div>
+                        
+ 						<div class="col-md-4"><label for="date">NOMBRE</label></div>
+						<div class="col-md-8"><input type="text"</div>
+						</div><br>
+						 <div class="btn-group">
+                         <div class="col-md-8"><button type="button" class="btn btn-primary">ACEPTAR</button>
+
+                        </div>
+						</form>
+						
+					</div>
 				@endif
 
-				@if($_SERVER['REQUEST_URI'] == "/adm/ingre")
-                   Ingreso de datos acad´emicos (e.g Cursos, asignaturas, estudiantes).		
+				@if($_SERVER['REQUEST_URI'] == "/encar/ingre" || $_SERVER['REQUEST_URI'] == "/encar/ingre/cursos") 
+                   <span style="color: #7292E9;"><h3>INGRESO DE DATOS ACADEMICOS</h3>
+                  <ul class="nav nav-pills">
+                  <li class="active"><a href="{{ url('encar/ingre')}}">Inicio</a></li>
+                  <li><a href="{{ url('encar/ingre/cursos')}}">CURSOS</a></li>
+                  <li><a href="{{ url('encar/ingre/asig')}}">ASIGNATURAS</a></li>
+                  <li><a href="{{ url('encar/ingre/estu')}}">ESTUDIANTES</a></li>
+
+                  </ul>
+                
+                @yield('content2')
            		@endif
 			</div>
 		</div>
