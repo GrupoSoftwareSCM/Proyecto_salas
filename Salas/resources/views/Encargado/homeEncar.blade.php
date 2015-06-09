444c7878fe16e565505e870587a77c251ff23517
jean pierre cid <jeanpierre.cid@gmail.com>
2015-06-09 02:47:05 -0300

    vista del CRUD del encargado de campus para los cursos faltan los controladores para conectarla con la base de datos

--- Salas/resources/views/Encargado/homeEncar.blade.php
+++ Salas/resources/views/Encargado/homeEncar.blade.php
@@ -15,15 +15,15 @@
 				@endif
 
 				@if($_SERVER['REQUEST_URI'] == "/encar/modif")
-					<h2>MODIFICAR SALAS</h2>
+					<h3>MODIFICAR SALAS</h3>
 					<div class="row">
 						 <br><br>
 						
-						
-					  	<form name="formulario" id="formulario" action="/demos/2013/05-html-form-datalist.php" method="POST">
+						<div class="col-md-8 col-md-offset-2"> 
+			            <form role="form" method="POST">
                         <div class="form-group">
-                        <div class="col-md-5"><b>SALA</b></div>
-                         <div class="col-md-7"> <datalist id="salas">
+                        <div class="col-md-8"><b>SALA</b></div>
+                         <div class="col-md-8"> <datalist id="salas">
                             <option value="M1....">
                             <option value="M2....">
                             <option value="M3....">
@@ -31,13 +31,20 @@
                          </datalist>
                     <input name="SALA" list="salas">
                         </div>
-                        <div>
-                        <div class="col-md-5"><label for="date">CAPACIDAD</label></div>
-                        <div class="col-md-7"><input type="number" value="1" min="1" max="50"></div>
-                        
- 						<div class="col-md-5"><label for="date">NOMBRE</label></div>
-						<div class="col-md-7"><input type="text"</div>
-						</div><br>
+                        <br><br><br>
+
+                        </div>
+                        <div class="form-group">
+                        <div class="col-md-8"><label for="date">CAPACIDAD</label></div>
+                        <div class="col-md-8"><input type="number" value="1" min="1" max="50"></div>
+                        </div>
+                        <br><br><br><br>
+                        <div class="form-group">
+ 						<div class="col-md-8"><label for="date">NOMBRE</label></div>
+						<div class="col-md-8"><input type="text"</div>
+						</div>
+						</div>
+						<br>
 						<br><br>
 						 <div class="btn-group">
 						 <br><br><br>
@@ -49,7 +56,7 @@
 					</div>
 				@endif
                     <!-- MENU INGRESAR DATOS ACADEMICOS -->
-				@if($_SERVER['REQUEST_URI'] == "/encar/ingre" || $_SERVER['REQUEST_URI'] == "/encar/ingre/cursos" || $_SERVER['REQUEST_URI'] == "/encar/ingre/asig" || $_SERVER['REQUEST_URI'] == "/encar/ingre/estu")
+				@if($_SERVER['REQUEST_URI'] == "/encar/ingre" || $_SERVER['REQUEST_URI'] == "/encar/ingre/cursos" || $_SERVER['REQUEST_URI'] == "/encar/ingre/asig" || $_SERVER['REQUEST_URI'] == "/encar/ingre/estu" || $_SERVER['REQUEST_URI'] == "/encar/ingre/estu/agre" || $_SERVER['REQUEST_URI'] == "/encar/ingre/estu/modi" || $_SERVER['REQUEST_URI'] == "/encar/ingre/elim" || $_SERVER['REQUEST_URI'] == "/encar/ingre/cursos/agre" || $_SERVER['REQUEST_URI'] == "/encar/ingre/cursos/modi" || $_SERVER['REQUEST_URI'] == "/encar/ingre/cursos/elim" || $_SERVER['REQUEST_URI'] == "/encar/ingre/asig/agre" || $_SERVER['REQUEST_URI'] == "/encar/ingre/asig/modi" || $_SERVER['REQUEST_URI'] == "/encar/ingre/asig/elim")
                                	<div class="panel-body">
 				  		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav nav-tabs">
@@ -78,6 +85,7 @@
 											<li><a href="{{url('/encar/ingre/cursos/elim')}}">Eliminar</a></li>
 										</ul>
 									</li>
+									@yield('CURSOS')
 								@endif
 								
 								@if($_SERVER['REQUEST_URI'] == "/encar/ingre/asig" || $_SERVER['REQUEST_URI'] == "/encar/ingre/asig/agre" || $_SERVER['REQUEST_URI'] == "/encar/ingre/asig/modi" || $_SERVER['REQUEST_URI'] == "/encar/ingre/elim")
@@ -136,8 +144,7 @@
 							</ul>
 						</div>
 				  	</div>
-                
-                @yield('content2')
+                 	@yield('content2')
                 @endif
            		
 			</div>
