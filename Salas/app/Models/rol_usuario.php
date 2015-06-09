7d979e4cc0b0f82f58e824055a9a2a479d929782
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2015-06-09 18:26:11 -0300

    creando controladores

---
+++ Salas/app/Models/rol_usuario.php
@@ -0,0 +1,15 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class rol_usuario extends Model {
+
+	protected $table = 'roles_usuarios'
+	protected $fillable = ['rut'];
+
+	public function rol()
+	{
+		return $this->belongsTo('rol','id_roles');
+	}
+
+}
