c5104af401a80acaeddc282f238aea59461f314f
jean pierre cid <jeanpierre.cid@gmail.com>
2015-08-05 23:51:33 -0300

    Ultimo cambio para alumno

--- Salas/resources/views/Estudiantes/homeAlum.blade.php
+++ Salas/resources/views/Estudiantes/homeAlum.blade.php
@@ -4,18 +4,37 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-2">
+        <div class="col-md-3">
             <!-- MENU -->
 
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    Menu
+
+            <nav class="navbar navbar-default" role="navigation">
+                <!-- El logotipo y el icono que despliega el menú se agrupan
+                     para mostrarlos mejor en los dispositivos móviles -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Desplegar navegación</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!--<a class="navbar-brand" href="#">Logotipo</a>-->
+                    {!! Html::image('/img/Logotipo_UTEM.png','logoTipo',['class'=>'navbar-brand']) !!}
                 </div>
-                <div class="panel-footer">
-                    <ul class="nav nav-sidebar">
-                        <li>{!!Html::linkRoute('estu.horario.index','Horario')!!}</li>
-                    </ul>
-                    
+
+                <!-- Agrupar los enlaces de navegación, los formularios y cualquier
+                     otro elemento que se pueda ocultar al minimizar la barra -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle glyphicon glyphicon-education" data-toggle="dropdown">
+                                Universidad<b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                      <li>{!!Html::linkRoute('estu.horario.index','Horario')!!}</li>
+                </div>
+               
                 </div>
             </div>
 
